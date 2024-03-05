7c1856751d6a5cbcceb983810835aa29084c8caa
ivangrisuk <grisuk.ia@gmail.com>
2024-03-05 15:37:42 +0300

    change: fix: рецепт add: update

--- update_programm.py
+++ update_programm.py
@@ -1,51 +1,72 @@
 import shutil
-from tkinter import *
-from tkinter import messagebox, Label, Frame
-
+import time
+import sqlite3 as sq
+from datetime import datetime
 import os
 import zipfile
 
-destination_dir = f'.{os.sep}Archive{os.sep}Загрузка_обновлений_БД{os.sep}' \
-                  f'{datetime.strftime(datetime.now(), "%d_%m_%y__%H_%M_%S")}{os.sep}'
 
-if not os.path.exists(path=destination_dir):
-    os.mkdir(path=destination_dir)
+def update():
+    path_srv = None
+    print('Извлечение пути к серверу')
+    with sq.connect(f"../генератор_справок{os.sep}data_base{os.sep}data_base.db") as conn:
+        cur = conn.cursor()
+        cur.execute("SELECT path_srv_data_base FROM app_data")
+        path_srv = cur.fetchone()
+        if isinstance(path_srv, tuple):
+            path_srv = path_srv[0]
+    if path_srv:
+        print('Извлечение пути к серверу - успешно')
+        edit_file = False
+        for file in sorted(os.listdir(path_srv)):
+            print(file)
+            if file.startswith('генератор_справок') and file.endswith('.zip'):
+                print("Обнаружен нужный файл:", file)
+                edit_file = file
+        if edit_file:
+            print("Копирую локальную базу данных")
+            if 'обновление' in os.listdir(f'..{os.sep}'):
+                shutil.rmtree(f'..{os.sep}обновление')
+            destination_dir = f'..{os.sep}обновление{os.sep}'
+            if not os.path.exists(path=destination_dir):
+                os.mkdir(path=destination_dir)
+            if 'генератор_справок' in os.listdir(f"..{os.sep}"):
+                if 'data_base' in os.listdir(f"..{os.sep}генератор_справок{os.sep}"):
+                    if not os.path.exists(path=f"{destination_dir}data_base{os.sep}"):
+                        os.mkdir(path=f"{destination_dir}data_base{os.sep}")
 
-destination_file = f'{destination_dir}' \
-                   f'Архив_{datetime.strftime(datetime.now(), "%d_%m_%y__%H_%M_%S")}.zip'
+                    for file in os.listdir(f"..{os.sep}генератор_справок{os.sep}data_base{os.sep}"):
+                        if '.db' in file:
+                            shutil.copy2(f"..{os.sep}генератор_справок{os.sep}data_base{os.sep}{file}",
+                                         f"{destination_dir}data_base{os.sep}{file}")
 
-if document := message.document:
-    await edit_message_text(message=message,
-                            text="Файл получен! Загружаю...")
-    for num in range(1, 4):
-        await edit_message_text(message=message,
-                                text=f"Попытка загрузки файла {num}")
-        try:
-            await document.download(destination_file=destination_file)
-        except Exception:
-            await edit_message_text(message=message,
-                                    text="Ошибка загрузки файла!")
-        else:
-            await edit_message_text(message=message,
-                                    text="Файл загружен! Открываю архив...")
+            print("Загружаю обновление")
+            destination_dir = f'..{os.sep}обновление{os.sep}'
 
-            archive = zipfile.ZipFile(destination_file, 'r')
+            shutil.copy2(f"{path_srv}{edit_file}",
+                         f"..{os.sep}обновление{os.sep}генератор_справок.zip")
 
-            archive.extractall(destination_dir)
-            await edit_message_text(message=message,
-                                    text="Архив успешно извлечен")
+            if 'генератор_справок' in os.listdir(f"..{os.sep}"):
+                print("Удаляю старую директорию")
+                shutil.rmtree(f'..{os.sep}генератор_справок')
+                # os.mkdir(path=f'..{os.sep}генератор_справок{os.sep}')
 
+            archive = zipfile.ZipFile(f"..{os.sep}обновление{os.sep}генератор_справок.zip", 'r')
+            archive.extractall(f"..{os.sep}")
             archive.close()
-            await edit_message_text(message=message,
-                                    text="Архив закрыт. Прохожусь по файлам")
+            print("Начинаю синхронизацию")
+            if 'data_base' in os.listdir(f"..{os.sep}обновление{os.sep}"):
 
-            await search_in_dir(message=message,
-                                path=destination_dir)
-            text = '<b>Прививки:</b>'
-            for file in vaccination_file_list:
-                text += f"\n{file.replace(f'.{os.sep}Archive{os.sep}Загрузка_обновлений_БД{os.sep}', '')}"
-            text += '\n\n<b>Картотека:</b>'
-            for file in card_file_list:
-                text += f"\n{file}.replace(f'.{os.sep}Archive{os.sep}Загрузка_обновлений_БД{os.sep}', '')"
-            await message.answer(text)
+                for file in os.listdir(f"..{os.sep}обновление{os.sep}data_base"):
+                    if '.db' in file:
+                        shutil.copy2(f"..{os.sep}обновление{os.sep}data_base{os.sep}{file}",
+                                     f"..{os.sep}генератор_справок{os.sep}data_base{os.sep}{file}")
+                print('База данных синхронизирована')
+
+
+if __name__ == '__main__':
+    update()
+    time.sleep(5)
+    os.system(f"start ..{os.sep}генератор_справок{os.sep}генератор_справок.exe")
+
 
--- Генератор_справок.py
+++ Генератор_справок.py
@@ -1505,7 +1505,7 @@ recipe_data = {
 	}
 }
 
-
+program_version = '2.1.4'
 # print("recipe_data = {")
 # for d_cat in all_data_diagnosis.get('drugs'):
 #     print(f"\t\"{d_cat}\":", '{')
@@ -9827,8 +9827,32 @@ def paste_log_in_root(root):
             log_in_root.update()
             data_base('activate_app')
 
-        time.sleep(1)
-        paste_frame_main(root)
+        if not user.get('error_connection'):
+            edit_file = False
+            path = user['app_data'].get('path_srv_data_base')
+            for file in sorted(os.listdir(path)):
+                print(file)
+                if file.startswith('генератор_справок') and file.endswith('.zip'):
+                    print("found!", file)
+
+                    if not program_version in file:
+                        print("found_edit!", file)
+                        edit_file = True
+            if edit_file:
+                result = messagebox.askyesno(title='Обновление',
+                                             message=f"Доступно обновление\n{edit_file}\nУстановить?")
+                print(result)
+                if result:
+                    os.system(f"start ..{os.sep}update_programm{os.sep}update_programm.exe")
+                    root.destroy()
+                else:
+                    paste_frame_main(root)
+
+            else:
+                paste_frame_main(root)
+        else:
+            time.sleep(1)
+            paste_frame_main(root)
         # log_in_root.quit()
 
     def is_valid__password(password):
@@ -10952,7 +10976,7 @@ def paste_frame_main(root):
 
 def main_root():
     root = Tk()
-    root.title('Генератор справок v_2.1.3')
+    root.title(f"Генератор справок v_{program_version}")
     root.config(bg="#36566d")
     root.geometry('+0+0')
 
